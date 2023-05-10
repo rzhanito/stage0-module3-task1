acfc9223abdcba5a7cc0f8ee80ad718fedb0f2ef
Erzhan <95659303+rzhanito@users.noreply.github.com>
2023-05-10 10:16:53 +0600

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,5 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        //write your code here
-    }
+        System.out.print("java"+"\nis"+"\nsmth");
 }
